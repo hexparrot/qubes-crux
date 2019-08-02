2ff59a07e0ebc834f3415ec77bf088c4355c172f
William Dizon <wdchromium@gmail.com>
2019-08-02 19:16:13 +0000

    +x and added qubes dns to resolv.conf

--- helpers/eth0_up.sh
+++ helpers/eth0_up.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-IPADDR=10.137.0.2
-GW=10.137.0.18
-DEV=eth0
+#IPADDR=10.137.0.2
+#GW=10.137.0.18
+#DEV=eth0
+#
+#ifconfig $DEV up
+#ip route add $GW dev $DEV
+#ip route add default via $GW
+#
+#ip addr add $IPADDR/32 dev $DEV
+#ip link set $DEV up
 
-ifconfig $DEV up
-ip route add $GW dev $DEV
-ip route add default via $GW
-
-ip addr add $IPADDR/32 dev $DEV
-ip link set $DEV up
+echo 'nameserver 10.139.1.1' >> /etc/resolv.conf
