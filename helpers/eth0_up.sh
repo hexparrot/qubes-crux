411f1ac0bf554baabdfc120995b109609b04fc6b
William Dizon <wdchromium@gmail.com>
2019-08-09 15:19:54 -0700

    provided instruction using eth0_up.sh

--- helpers/eth0_up.sh
+++ helpers/eth0_up.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-IPADDR=10.137.0.2
-GW=10.137.0.18
+IP=${IP:?"Provide env variable: IP=10.137.0.x"}
+GW=${GW:?"Provide env variable: GW=10.137.0.y"}
 DEV=eth0
 
 ifconfig $DEV up
 ip route add $GW dev $DEV
 ip route add default via $GW
 
-ip addr add $IPADDR/32 dev $DEV
+ip addr add $IP/32 dev $DEV
 ip link set $DEV up
 
 echo 'nameserver 10.139.1.1' >> /etc/resolv.conf
+
