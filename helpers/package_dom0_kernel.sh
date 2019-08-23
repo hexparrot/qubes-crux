89295ed9637f6baa68cf45aeb2c445fe686a36de
William Dizon <wdchromium@gmail.com>
2019-08-23 10:57:22 -0700

    instructions for PVH/PV kernel

---
+++ helpers/package_dom0_kernel.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+KERN_VERSION=$(uname -r)
+
+mkdir /tmp/$KERN_VERSION
+pushd /tmp/$KERN_VERSION
+
+: | gzip > initramfs
+cp /boot/vmlinuz-$KERN_VERSION vmlinuz
+cp /boot/config-$KERN_VERSION .
+
+echo 'root=/dev/xvda3 ro nomodeset console=hvc0 xen_scrub_pages=0' > default-kernelopts-common.txt
+popd
+
+tar -C /tmp -czf kernel.tar.gz $KERN_VERSION
+rm -rf /tmp/$KERN_VERSION
+
